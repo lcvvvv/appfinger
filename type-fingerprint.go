2363c0944fe31872fff9d8929ce85893c7aecac8
chegn.kv2@gmail.com <cheng.kv2@gmail.com>
2022-11-07 15:16:38 +0800

    [+]v0.1.1 init

--- type-fingerprint.go
+++ type-fingerprint.go
@@ -1,64 +1,18 @@
 package appfinger
 
-import (
-	"errors"
-	"fmt"
-	"regexp"
-	"strings"
-)
-
 type FingerPrint struct {
-	//指纹适用范围
-	//Protocol string
-	//Path     string
-
-	//指纹适配产品
-	ProductName string
-
-	//指纹识别规则
-	Keyword *Expression
+	ProductName []string
+	Hostname    string
+	Domain      string
+	MACAddr     string
 }
 
-var regxKeywordFinger = regexp.MustCompile(`^([^ \t]+)[ \t](.*)$`)
+var emptyProductName []string
 
-func NewFingerPrint(s string) (*FingerPrint, error) {
-	//去除表达式尾部空格
-	s = strings.TrimSpace(s)
-	//对表达式合法性进行校验
-	if regxKeywordFinger.MatchString(s) == false {
-		return nil, errors.New(" syntax error :" + s)
-	}
-	//校验合法，序列化表达式
-	i := regxKeywordFinger.FindStringSubmatch(s)
-	productName := i[1]
-	expressionString := i[2]
-	expression, err := NewExpression(expressionString)
-	if err != nil {
-		return nil, err
-	}
-
-	return &FingerPrint{
-		//指纹适用范围
-		//Protocol: "",
-		//Path:     "",
-		//指纹适配产品
-		ProductName: productName,
-		//指纹识别规则
-		//Icon:    "",
-		//Hash:    "",
-		Keyword: expression,
-	}, nil
+func New() *FingerPrint {
+	return &FingerPrint{emptyProductName, "", "", ""}
 }
 
-func (f *FingerPrint) Match(banner *Banner) string {
-	if f.Keyword != nil {
-		if f.Keyword.Match(banner) {
-			fmt.Println(f.Keyword.MakeBoolExpression(banner))
-			fmt.Println(f.Keyword.value)
-			fmt.Println(f.Keyword.expr)
-			fmt.Println(f.ProductName)
-			return f.ProductName
-		}
-	}
-	return ""
+func (f *FingerPrint) AddProduct(productName string) {
+	f.ProductName = append(f.ProductName, productName)
 }
